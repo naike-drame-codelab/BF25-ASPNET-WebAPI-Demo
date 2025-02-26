5a76ada2ed6a60034bf4f667ca7b033ab7b3fb64
Naïké Dramé <naike.drame.dev@gmail.com>
2025-02-26 09:49:44 +0100

    added services extensions

---
+++ Refuge.API/Configurations/ServicesExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Refuge.BLL.Interfaces.Repositories;
+using Refuge.BLL.Interfaces.Services;
+using Refuge.BLL.Services;
+using Refuge.DAL.Repositories;
+
+namespace Refuge.API.Configurations
+{
+    public static class ServicesExtensions
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ICatRepository, CatRepository>();
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICatService, CatService>();
+        }
+    }
+}
--- Refuge.API/Program.cs
+++ Refuge.API/Program.cs
@@ -1,12 +1,7 @@
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Refuge.BLL.Enums;
-using Refuge.BLL.Interfaces.Repositories;
-using Refuge.BLL.Interfaces.Services;
-using Refuge.BLL.Services;
+using Refuge.API.Configurations;
 using Refuge.DAL;
-using Refuge.DAL.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,8 +17,8 @@ builder.Services.AddDbContext<RefugeContext>(
     b => b.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
 );
 
-builder.Services.AddScoped<ICatService, CatService>();
-builder.Services.AddScoped<ICatRepository, CatRepository>();
+builder.Services.AddRepositories();
+builder.Services.AddServices();
 
 var app = builder.Build();
 
